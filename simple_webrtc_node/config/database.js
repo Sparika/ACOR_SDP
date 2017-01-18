1cdd43dfbaa107d218ff2b49c5caf2d5f1e53316
Kevin CORRE <kevin1.corre@orange.com>
2017-01-18 16:49:29 +0100

    prod/dev env

--- simple_webrtc_node/config/database.js
+++ simple_webrtc_node/config/database.js
@@ -1,6 +1,12 @@
 // config/database.js
+env = process.env.NODE_ENV || 'development';
+
 module.exports = {
+    development: {
+        'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    },
+    production: {
+        'url' : 'mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT
+    }
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
+}[env];
