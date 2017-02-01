75677d2685f0adadc9e3e4851f238d7f7eff9b01
Kevin CORRE <kevin1.corre@orange.com>
2017-02-01 18:58:44 +0100

    uncommented adapter WebRTC

--- webrtc_service/public/javascripts/main.js
+++ webrtc_service/public/javascripts/main.js
@@ -9,18 +9,31 @@ var remoteStream;
 var turnReady;
 
 var pcConfig = {
-  'iceServers': [{
+  'iceServers': [
+  {
     'url': 'stun:stun.l.google.com:19302'
-  },
-  {
-    "url": "stun:stun.sipgate.net"
-  },
-  {
-    "url": "stun:217.10.68.152"
-  },
-  {
-    "url": "stun:stun.sipgate.net:10000"
-  }
+  }//,
+//  {
+//    'url': 'stun:stun1.l.google.com:19302'
+//  },
+//  {
+//    'url': 'stun:stun2.l.google.com:19302'
+//  },
+//  {
+//    'url': 'stun:stun3.l.google.com:19302'
+//  },
+//  {
+//    'url': 'stun:stun4.l.google.com:19302'
+//  },
+//  {
+//    "url": "stun:stun.sipgate.net"
+//  },
+//  {
+//    "url": "stun:217.10.68.152"
+//  },
+//  {
+//    "url": "stun:stun.sipgate.net:10000"
+//  }
   ]
 };
 
