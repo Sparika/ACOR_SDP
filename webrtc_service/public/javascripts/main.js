b0b22082e0645b6a31032571cbbd5946838e9889
Kevin CORRE <kevin1.corre@orange.com>
2017-02-01 19:03:20 +0100

    added pcconfig

--- webrtc_service/public/javascripts/main.js
+++ webrtc_service/public/javascripts/main.js
@@ -223,7 +223,7 @@ window.onbeforeunload = function() {
 
 function createPeerConnection() {
   try {
-    pc = new RTCPeerConnection(null);
+    pc = new RTCPeerConnection(pcConfig);
     pc.onicecandidate = handleIceCandidate;
     pc.onaddstream = handleRemoteStreamAdded;
     pc.onremovestream = handleRemoteStreamRemoved;
