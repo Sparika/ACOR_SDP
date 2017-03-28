af54039ecca2a8818ca730bf96f7a8caebaaa127
IdirB <idir.bouaoud92@gmail.com>
2017-03-28 14:12:29 +0200

    changed

--- webrtc_service/app/models/idAssertion.js
+++ webrtc_service/app/models/idAssertion.js
@@ -1,3 +1,40 @@
-/**
- * Created by root on 22/03/17.
- */
+
+
+var mongoose   = require ('mongoose');
+
+var User       = require('./user');
+
+
+var idAssertion = mongoose.Schema({ // data schemas
+    user: {type:String ,required:true},
+    content: {type:String, required : true}
+});
+
+var assertion = module.exports = mongoose.model('assertion',idAssertion);
+
+// add assertion
+        module.exports.addAssertion = function (ass,callback) {
+        assertion.create(ass,callback);
+};
+
+// get assertion
+    module.exports.getAssertion = function (callback, limit) {
+        assertion.find(callback).limit(limit);
+};
+
+// get assertion ById
+
+    module.exports.getAssertionById = function (id, callback) {
+        assertion.findOne(id,callback);
+
+        //var users = user.find();
+
+    };
+    /*module.exports.getAssertionById = function (id, callback) {
+        assertion.findById(id,callback);
+
+};
+*/
+
+
+
