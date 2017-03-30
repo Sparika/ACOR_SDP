f86c78c1a260d3a12d3f70eb4f7d3d4aade682ae
IdirB <idir.bouaoud92@gmail.com>
2017-03-30 11:31:49 +0200

    change

--- webrtc_service/.well-known/idp-proxy/try2.js
+++ webrtc_service/.well-known/idp-proxy/try2.js
@@ -1,113 +1,111 @@
-/**
- * Created by root on 27/03/17.
- */
 
+    function getAssertion(){
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+        fetch('http://localhost:4041/assertion/', {
+            method: 'get'
+        }).then(function(response) {
 
-function getAssertion(){
+            return response.json
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
+        }).catch(function(err) {
+            //'Error :(
+        });
+    }
 
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
+    function getAssertionById(id){
 
-            var res = JSON.parse(xhttp.responseText);
-            console.log(res);
-        }
-    };
-    xhttp.open("GET", 'http://localhost:4041/assertion' , true);
-    xhttp.send();
-}
+        fetch('http://localhost:4041/assertion/'+id, {
+            method: 'get'
+        }).then(function(response) {
+            return response
 
-function getAssertionById(){
+        }).catch(function(err) {
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
-
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-
-            var res = JSON.parse(xhttp.responseText);
-            console.log(res);
-        }
-    };
-    xhttp.open("GET", 'http://localhost:4041/assertion/:id' , true);
-    xhttp.send();
-}
-
-function addAssertion(){
-
-    var data = {"content": "im john smith"};
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-
-            //console.log(JSON.stringify(data));
-        }
-    };
-    xhttp.open("POST", "http://localhost:4041/assertion");
-    xhttp.setRequestHeader("cache-control", "no-cache");
-    xhttp.setRequestHeader("content-type", "application/json");
-    xhttp.send(JSON.stringify(data));
-}
-
-addAssertion();
-//console.log('addAssertion');
-//console.log(addAssertion());
-//console.log(getAssertionById());
-console.log(getAssertion());
-
-var idp = {
-
-generateAssertion: (contents , origin) => {
-
-    // TODO : sign contents in the Id Token
-
-    return new Promise((resolve, reject) =>
-
-    //add assertion
-        addAssertion()
-
-            .then(asser => {
-                console.log('assertion added '+ asser)
-                .catch(error => {
-                        console.log('not assertion found')
-                })
-            })
-
-    )},
-
-    validateAssertion: (assertion, origin) => {
-
-        //TODO there is probably a better way to do that?
-
-        // return assertion
-        return getAssertionById()
-
-            .then(result => {
-                if (!result) return Promise.reject('Invalid signature on identity assertion') //reject
-
-                else {
-
-                    // on va retouner l'id de l'assertion de la base de donnée ?
-
-                    // je pense il faut une methode getAssertionBycontent?
-                    return Promise.resolve('ok') //resolve
-                }})
-            .catch(error => reject({'no match':+error}));
+            //'Error :(
+        });
 
     }
 
+    function addAssertion(content){
 
-};
+        fetch('http://localhost:4041/assertion/', {
+            method: 'POST',
+            headers: {
+                //'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "content": content
+            })
 
-if (typeof rtcIdentityProvider != 'undefined') {
-    rtcIdentityProvider.register(idp);
-    console.log("Proxy loaded")
-} else {
-    console.warn('IdP not running in the right sandbox');
-}
+        }).then(function (data) {
+            console.log('Request success: ', data);
+            return data
+        })
+            .catch(function (error) {
+                console.log('Request failure: ', error);
+            });
+
+    }
+
+    /*
+    ******************************
+    Generate & Validate assertion
+    ******************************
+     */
+
+
+
+    var idp = {
+
+        generateAssertion: (contents , origin) => {
+
+            return  new Promise(function (resolve, reject) {
+
+                // call addAssertion
+                addAssertion();
+
+            }).then(function(data) {
+                /*
+                 * specification
+                 *  DomString*/
+
+                JSON.stringify(data);
+
+            }).catch(function(err) {
+                /* error :( */
+                console.log('error')
+                throw err;
+            })
+
+
+        },
+
+
+        validateAssertion: (assertion, origin) => {
+
+            // return assertion
+            getAssertionById(assertion)
+
+                .then(function(response) {
+                return Promise.resolve();
+
+
+                }).catch(function(err) {
+
+                console.log(err)
+            })
+
+        }
+
+    };
+
+
+    if (typeof rtcIdentityProvider != 'undefined') {
+        rtcIdentityProvider.register(idp);
+        console.log("Proxy loaded")
+    } else {
+        console.warn('IdP not running in the right sandbox');
+    }
 
 
 
