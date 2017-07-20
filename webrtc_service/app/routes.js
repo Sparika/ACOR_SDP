d59d1fb608ae0322db2e887532a97f9959906e90
IdirB <idir.bouaoud92@gmail.com>
2017-07-20 14:46:09 +0200

    authority

--- webrtc_service/app/routes.js
+++ webrtc_service/app/routes.js
@@ -4,7 +4,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var fs = require('fs');
-
+var spit = require('/root/IdeaProject/ACOR_SDP/webrtc_service/app/models/spit.js');
+var zone = require('/root/IdeaProject/ACOR_SDP/webrtc_service/app/models/zone.js');
+var auth = require('/root/IdeaProject/ACOR_SDP/webrtc_service/app/models/authority.js');
+var prop =0;
+mongoose.Promise = global.Promise;
 
 module.exports = function (app, passport) {
     // local ipdProxy routes ===============================================================
@@ -100,7 +104,7 @@ module.exports = function (app, passport) {
 
         // process the login form
         app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
+            successRedirect : '/', // redirect to the secure profile section
             failureRedirect : '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
         }));
@@ -207,19 +211,39 @@ module.exports = function (app, passport) {
         return userInRoom
     }
 
+
+    var rooms1 = {user:'',name:'',descr:'',socket:[]}
+
         // process the login form
     app.post('/room/create', isLoggedIn, function(req, res) {
+
         var rooms = app.get('rooms'),
             roomId = req.body.room,
-            descr = req.body.descr
-        if(! rooms[roomId]){
+            descr = req.body.descr;
+        console.log('display information post room')
+        console.log('POST ROOM ID = '+roomId+'description = '+ descr);
+        // create object instance for room
+
+
+        if (roomId !== (null && undefined)){
+        rooms1 = {
+            user  : [],
+            name  : roomId,
+            descr : req.body.descr,
+            socket:[]
+        }
+
+        console.log('the name of room is '+rooms1.name+'the description'+' '+rooms1.descr)
+        rooms = rooms1;
+
+        /*if(!rooms[roomId]){
             console.log('Create '+roomId)
             rooms[roomId] = {
                 user:[],
                 name: roomId,
-                socket:[],
-                descr:descr
-            }
+                descr:descr,
+                socket:[]
+            }*/
             res.redirect('/room/'+roomId)
         } else {
             req.flash('roomMessage', 'Room '+roomId+' already exists.');
@@ -228,18 +252,29 @@ module.exports = function (app, passport) {
     });
 
 
+
+
+
+
     // ROOM ==============================
     app.get('/room/:roomId', isLoggedIn, function(req, res){
         var rooms = app.get('rooms'),
             roomId = req.params.roomId,
-            user = req.user
+            user = req.user,
+            descr = rooms1.descr;
+
+        console.log(rooms1);
+
         if(! rooms[roomId]){
             console.log('Get '+roomId)
             rooms[roomId] = {
                 user:[],
                 name: roomId,
+                descr:descr,
                 socket:[]
             }
+
+            console.log('the last'+rooms[roomId])
         }
         if(rooms[roomId].user.length<2){
             rooms[roomId].user.push(user)
@@ -255,17 +290,134 @@ module.exports = function (app, passport) {
         }
     });
 
-    app.delete('/room/:roomId', isLoggedIn, function(req,res){
-        var rooms = app.get('rooms')
-            room = rooms[req.params.roomId],
-            index = rooms.indexOf(req.params.roomId)
 
-        rooms.splice(index,1)
+
+    app.post('/room/spit', isLoggedIn, function(req, res){
+
+        var request = req.body;
+        console.log(request);
+        //var request2 = JSON.parse(request)
+        //console.log(request2);
+        spit.addspit(request);
+        console.log('zoning');
+        //spit.getspitcall();
+        // calculate probability
+
+    });
+
+    app.post('/room/authority', isLoggedIn, function(req, res){
+
+        var request = req.body;
+        console.log(request);
+        //var request2 = JSON.parse(request)
+        //console.log(request2);
+        auth.addauthority(request);
+        // get the service
+
+        /*var authority_param = auth.getservice2();
+         console.log(item.service);
+         console.log(item.score)*/
+
+        auth.get_score_service();
+
+
+        //
+
+    });
+
+
+    app.get('/room/call/probability', function(req,res){
+
+        var promise = auth.find({},{_id:0, score:1}).limit(1).sort({$natural:-1}).exec();
+
+        promise.then(function(array) {
+            // array of people ... do what you want here...
+            console.log(array[0]);
+            //res.send(JSON.stringify(arrayOfPeople[0]));
+            return res.end(JSON.stringify(array[0].score))
+
+        });
+
+    });
+
+    app.post('/room/zone', isLoggedIn, function(req, res){
+        var request = req.body;
+        console.log(request);
+        zone.addzone(request);
+    });
+
+
+    app.get('/room/zone/Black', function(req,res){
+
+        var obj  = {};
+        var promise  = zone.find({'zone.Black': true}).distinct('service').exec();
+        promise.then(function(doc) {
+            if (doc[0]!== undefined){
+            obj ={service: doc[0] , Grey:false, Black:true};
+            return res.end(JSON.stringify(obj));
+            }
+        });
+
+        var promise2 = zone.find({'zone.Grey': true}).distinct('service').exec()
+        promise2.then(function(doc) {
+            if (doc[0]!== undefined){
+                obj ={service: doc[0] , Grey:true, Black:false};
+                return res.end(JSON.stringify(obj));
+            }
+        })
+
+
+        var promise3 = zone.find({}).distinct('service').exec()
+        promise3.then(function(doc) {
+            if (doc[0]=== undefined){
+                obj ={ Grey:false, Black:false};
+                return res.end(JSON.stringify(obj));
+            }
+
+        });
+
+    });
+
+
+
+   /* app.delete('/room/:roomId', isLoggedIn, function(req,res){
+        var rooms = app.get('rooms');
+            room = rooms[req.params.roomId],
+            //index = rooms.indexOf([req.params.roomId])
+        //rooms.splice(room)
+        console.log('i try to remove item from room! list***')
+        //console.log('room'+rooms[room].name)
+        console.log('stringfy*******************')
+        console.log(rooms)
+
+
+        //delete rooms.na
+
+        console.log('removed from list****')
+
         res.sendStatus(200)
         for (var i = 0; i<room.socket.length; i++) {
             room.socket[i].emit('deleted', req.params.roomId)
         }
+    });*/
+
+
+
+    app.delete('/room/:roomId', isLoggedIn, function(req,res){
+        var rooms = app.get('rooms')
+        var Id = req.params.roomId;
+        delete rooms[Id];
+        //console.log('DELETE ROOM BY ID RESULT'+JSON.stringify(rooms));
+        res.sendStatus(200);
+        for (var id in rooms.socket){
+            rooms.socket[id].emit('deleted', req.params.roomId)
+
+        }
     });
+
+
+
+
 
     app.delete('/room/:roomId/user/me', isLoggedIn, function(req,res){
         res.sendStatus(200)
@@ -282,9 +434,14 @@ module.exports = function (app, passport) {
     })
 };
 
+
+
+
+
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
     res.redirect('/');
 }
+
